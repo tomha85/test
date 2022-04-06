9c93e06d551aa243981d9939cd5b9314e688c9d8
Ubuntu <webvm@webvm.g05kk1vij2uuhcwkddl2emki0c.cx.internal.cloudapp.net>
2022-04-06 00:53:53 +0000

    change html

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ from FlaskExercise import app
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = int(environ.get('SERVER_PORT', '5553'))
     except ValueError:
-        PORT = 5555
+        PORT = 5553
     app.run(HOST, PORT, debug=True)
